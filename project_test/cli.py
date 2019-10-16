46fa55403df4e2930392bed6ed4da17244b1574a
catana-research <38889032+catana-research@users.noreply.github.com>
2019-10-16 21:40:11 +0100

    Remove CLI

--- project_test/cli.py
+++ project_test/cli.py
@@ -2,15 +2,14 @@
 
 """Console script for project_test."""
 import sys
-import click
 
 
 @click.command()
 def main(args=None):
     """Console script for project_test."""
-    click.echo("Replace this message by putting your code into "
-               "project_test.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    #click.echo("Replace this message by putting your code into "
+    #           "project_test.cli.main")
+    #click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
 
--- tests/test_project_test.py
+++ tests/test_project_test.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from click.testing import CliRunner
+#from click.testing import CliRunner
 
 from project_test import project_test
 from project_test import cli
@@ -25,14 +25,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'project_test.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
