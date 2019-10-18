91dddef7ac735badd1318c868f7c2c039f503525
catana-research <38889032+catana-research@users.noreply.github.com>
2019-10-18 21:10:03 +0100

    Fix docstring.

--- project_test/project_test.py
+++ project_test/project_test.py
@@ -40,5 +40,5 @@ class Cat(object):
         return meow_str
 
     def purr(self):
-        """ n_{\mathrm{happiness}} = \sum_{k=0}^{N-1} hug_k meow_k                                                              """
+        """Purr like a cat"""
         return self.purr_sound
--- tests/test_cat.py
+++ tests/test_cat.py
@@ -1,5 +1,5 @@
 import pytest
-from   project_test.project_test import Cat
+from project_test.project_test import Cat
 
 """Tests for `project_test` package."""
 
@@ -33,4 +33,3 @@ def test_meow(test_input, expected):
     cat = Cat()
     print(*test_input)
     assert cat.meow(*test_input) == expected, 'Cat Meow is of incorrect format'
-
