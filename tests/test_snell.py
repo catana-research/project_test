79251d308d7987911817a809092c96a48fb13fb2
catana-research <38889032+catana-research@users.noreply.github.com>
2019-10-19 15:08:14 +0100

    Parameterise test.

--- tests/test_snell.py
+++ tests/test_snell.py
@@ -1,17 +1,15 @@
 # example/tests/test_examples.py
 import numpy as np
+import pytest
 from project_test.snell import snell
 
 
-def test_perpendicular():
+@pytest.mark.parametrize('n1, n2', [(2.00, 3.00), (3.00, 2.00),])
+def test_perpendicular(n1, n2):
     # For any indexes, a ray normal to the surface should not bend.
     # We'll try a couple different combinations of indexes....
 
-    actual = snell(0, 2.00, 3.00)
-    expected = 0
-    assert actual == expected
-
-    actual = snell(0, 3.00, 2.00)
+    actual = snell(0, n1, n2)
     expected = 0
     assert actual == expected
 
