5263e071e3fbad7d6aeb7311e83a51206b55f0c1
catana-research <38889032+catana-research@users.noreply.github.com>
2019-10-19 21:03:47 +0100

    Add fixture example.

--- tests/test_snell.py
+++ tests/test_snell.py
@@ -19,3 +19,29 @@ def test_air_water():
     actual = snell(np.pi/4, n_air, n_water)
     expected = 0.5605584137424605
     assert np.allclose(actual, expected)
+
+# Fixtures are used to setup test data that is used frequently
+@pytest.fixture
+def peak():
+    # Construct a 1-dimensional Gaussian peak.
+    x = np.linspace(-10, 10, num=21)
+    sigma = 3.0
+    peak = np.exp(-(x / sigma)**2 / 2) / (sigma * np.sqrt(2 * np.pi))
+    return peak
+
+
+def test_gaussian_mean(peak):
+    actual = np.mean(peak)
+    expected = 0.04759819937913877
+    assert np.allclose(actual, expected)
+
+
+def test_gaussian_std(peak):
+    actual = np.std(peak)
+    expected = 0.04703300841553288
+    assert np.allclose(actual, expected)
+
+
+def test_gaussian_positive(peak):
+    # Test that there are no negative values.
+    assert np.all(peak >= 0)
